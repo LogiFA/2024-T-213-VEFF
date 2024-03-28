9fe9362cc874d242ba3fa7c641229786d4e2fef6
LogiFA <logi23@ru.is>
2024-03-28 18:40:34 +0000

    Update index.test.js

--- assignments/assignment4_supplement_material/assignment4_starter_pack/test/index.test.js
+++ assignments/assignment4_supplement_material/assignment4_starter_pack/test/index.test.js
@@ -6,14 +6,37 @@ const request = require("supertest");
 const app = require("../index");
 
 describe("Endpoint tests", () => {
-  // Make sure the server is in default state when testing
   beforeEach(async () => {
+    // Reset the server state before each test to ensure test isolation
     await request(app).get("/api/v1/reset");
   });
-
+  
   /*---------------------------
    Write your tests below here
   ---------------------------*/
+
+  it("GET /events - should return all events", async () => {
+    const response = await request(app).get("api/v1/events");
+    // TODO check that response body is there and is an array
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toHaveLength(3);
+  })
+
+  it("Post /events - should create a new event", async () => {
+    const response = (await request(app).post("api/v1/events")).body();
+    expect(response.statusCode).toBe(200);
+    // expect(response.body).toHaveProperty('id', 4)
+    // expect(response.body).toHaveProperty('name', "my new event")
+    // expect(response.body).toHaveProperty('date', "2024-05-01")
+    // expect(response.body).toHaveProperty('location', "harpa")
+    // expect(response.body).toMatchObject({
+    //   id: 4,
+    //   name : "my new event",
+    //   date : "2024-05-01",
+    //   location : "harpa"
+    // })
+  });
+
 
   // Try to call and endpoint that does not exists
   it("Example Test: should return a 404 status for a non-existent endpoint", async () => {
