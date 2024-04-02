23aebcaed2ab443df9a5fd64b2ba0e1e8eb2536a
LogiFA <logi23@ru.is>
2024-04-02 16:17:28 +0000

    update index.test

--- assignments/assignment4_supplement_material/assignment4_starter_pack/test/index.test.js
+++ assignments/assignment4_supplement_material/assignment4_starter_pack/test/index.test.js
@@ -16,7 +16,7 @@ describe("Endpoint tests", () => {
   ---------------------------*/
 
 
-  it("GET /events - should return all events", async () => {
+  it("GET /books - should return all events", async () => {
     const response = await request(app).get("/api/v1/books");
     expect(response.statusCode).toBe(200);
     expect(response.body).toBeDefined();
@@ -24,6 +24,19 @@ describe("Endpoint tests", () => {
     expect(response.body.length).toBeGreaterThan(0);
   });
   
+  it("GET /api/v1/genres/:genreId/books/:bookId get certain book", async () => {
+    const genreId = 1;
+    const bookId = 1;
+    const response = await request(app).get(`/api/v1/genres/${genreId}/books/${bookId}`);
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toBeDefined();
+    expect(response.body).toEqual(expect.objectContaining({
+      id: expect.any(Number),
+      title: expect.any(String),
+      author: expect.any(String),
+      genreId: genreId,
+    }));
+  });
   //   id: 4,
   //   name : "my new event",
   //   date : "2024-05-01",
@@ -91,7 +104,42 @@ describe("Endpoint tests", () => {
     expect(response.body.message).toBe(`Book with id ${bookId} does not exist`);
   });
 
-  
+  it("POST /events should fail when the request body does not contain the author property", async () => {
+    const genreId = 1;
+    const bookData = {
+      title: "A Book Without an Author",
+    };
+    const response = await request(app).post(`/api/v1/genres/${genreId}/books/`).send(bookData);
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toBeDefined();
+    expect(response.body.message).toContain("Missing required book fields within the body (title, author).");
+  });
+
+  it("POST /api/v1/genres should fail when missing the correct authorization", async () => {
+    const newGenre = {
+      name: "New Genre",
+    };
+    const response = await request(app).post(`/api/v1/genres`).send(newGenre);
+    expect(response.statusCode).toBe(401);
+    expect(response.body).toBeDefined();
+    expect(response.body.message).toBe("Unauthorized");
+  });
+
+  it("PATCH /genre - should return 201" , async () => {
+    const newGenre = {
+      name: "New Genre",
+    };
+
+    const HMAC = "d5951928a797e3de418978abeb1c4f036672aa63b3241843493bfae1c0e60923"
+
+    const response = await request(app)
+      .post('/api/v1/genres')
+      .set('Authorization', `HMAC ${HMAC}`)
+      .send(newGenre)
+      
+    expect(response.statusCode).toBe(201);
+    expect(response.body).toBeDefined();
+  });
 
   // Try to call and endpoint that does not exists
   it("Example Test: should return a 404 status for a non-existent endpoint", async () => {
